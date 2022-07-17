370bb200ede1aac7d8738b4403ae983df6948a40
Dmitriy <Dimitriy303@yandex.ru>
2022-07-18 03:35:30 +0900

    ready

--- src/main/java/service/CalculatorService.java
+++ src/main/java/service/CalculatorService.java
@@ -4,17 +4,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
-    public int plus (int a, int b){
-return a + b;
+    public int plus(int a, int b) {
+        return a + b;
     }
-    public int minus (int a, int b){
-return a - b;
+
+    public int minus(int a, int b) {
+        return a - b;
     }
-    public int multiply (int a, int b){
-return a * b;
+
+    public int multiply(int a, int b) {
+        return a * b;
     }
-    public double divide (int a, int b){
-return (double) a/b;
+
+    public double divide(int a, int b) {
+        return (double) a / b;
     }
 
 }
